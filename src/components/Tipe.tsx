5290f8c193e7e26ea9e91212c25a9c9ebf621263
rtr-dnd <skysoyn@gmail.com>
2020-10-30 02:27:49 +0900

    :sparkles: mobile horizontal scroll

--- src/components/Tipe.tsx
+++ src/components/Tipe.tsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line
-import React, { useEffect, useState, RefObject } from 'react'
+import React, { useEffect, useState, RefObject, useRef } from 'react'
 import Editor from 'tipe-markdown-editor'
 import { useSelector, useDispatch } from 'react-redux'
 import styled from 'styled-components'
@@ -25,19 +25,26 @@ import IconAddThread from './icons/IconAddThread'
 import IconThreadMore from './icons/IconThreadMore'
 import IconForward from './icons/IconForward'
 import { Redirect } from 'react-router-dom'
-import { spawn } from 'child_process'
+import breakpoint from 'styled-components-breakpoint'
 
 const TipeContainer = styled.div`
   padding: 0 32px;
   display: flex;
   border-bottom: 1px solid ${props => props.theme.border};
   transition: 0.5s;
+  ${breakpoint('mobile', 'tablet')`
+    position: relative;
+    padding: 0;
+  `}
 `
 const Texts = styled.div`
   flex-grow: 1;
   flex-shrink: 1;
   min-width: 0;
   margin: 48px 0;
+  ${breakpoint('mobile', 'tablet')`
+    display: none;
+  `}
   p, div {
     font-size: 14px;
     line-height: 1.8em;
@@ -50,11 +57,47 @@ const Texts = styled.div`
     }
   }
 `
+const MobileTexts = styled.div`
+  overflow-x: scroll;
+  width: 100%;
+  margin: 48px 0;
+  padding-bottom: 80px;
+  display: flex;
+  background-color: transparent;
+  z-index: 1;
+  scroll-snap-type: x mandatory;
+  ${breakpoint('tablet')`
+    display: none;
+  `}
+  p, div {
+    font-size: 14px;
+    line-height: 1.8em;
+    transition: 0.5s;
+    background: transparent;
+    color: ${props => props.theme.text};
+    &.placeholder:before {
+      color: ${props => props.theme.textGrey};
+      transition: 0.5s;
+    }
+  }
+`
+const MobileEditorWrap = styled.div`
+  min-width: 100%;
+  transition: 0.5s;
+  background-color: ${props => props.theme.background} !important;
+  scroll-snap-align: start;
+`
+const MobileTextsSpacer = styled.div`
+  pointer-events: none;
+  min-width: 80%;
+  scroll-snap-align: end;
+`
 const ThreadBar = styled.div`
-  padding-bottom: 16px;
+  padding: 8px 12px;
   cursor: pointer;
-  margin-bottom: 24px;
-  border-bottom: 1px solid ${props => props.theme.border};
+  margin-bottom: 16px;
+  border: 1px solid ${props => props.theme.border};
+  border-radius: 8px;
   display: flex;
   align-items: center;
   color: ${props => props.theme.textGrey};
@@ -68,11 +111,13 @@ const ThreadBar = styled.div`
   }
   p {
     margin: 0 0 0 8px;
+    font-size: 13px;
     color: ${props => props.theme.textGrey};
     white-space: nowrap;
   }
   div {
     margin: 0;
+    font-size: 13px;
     color: ${props => props.theme.textGrey};
     overflow: hidden;
     display: flex;
@@ -100,6 +145,15 @@ const Titles = styled.div`
   flex-direction: column;
   text-align: right;
   align-items: flex-end;
+  ${breakpoint('mobile', 'tablet')`
+    position: absolute;
+    pointer-events: none;
+    width: 100%;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    z-index: 5;
+  `}
   .hiding {
     transition: opacity 0.3s;
     opacity: 0.4;
@@ -133,6 +187,42 @@ const Sticky = styled.div`
   bottom: 80px;
   display: flex;
   flex-direction: column-reverse;
+  ${breakpoint('mobile', 'tablet')`
+    display: none;
+  `}
+`
+const MobileStickyBottom = styled.div`
+  position: sticky;
+  bottom: 48px;
+  display: flex;
+  padding: 0 12px 12px 12px;
+  flex-direction: column-reverse;
+  background-color: ${props => props.theme.backgroundTransparent};
+  transition: 0.5s;
+  pointer-events: all;
+  ${breakpoint('tablet')`
+    display: none;
+  `}
+`
+const MobileTitlesUnder = styled(Titles)`
+  z-index: 0;
+  ${breakpoint('tablet')`
+    display: none;
+  `}
+`
+const MobileStickyMiddle = styled.div`
+  opacity: 0;
+  transform: scale(0.8);
+  width: 80%;
+  position: sticky;
+  top: 120px;
+  display: flex;
+  flex-direction: column-reverse;
+  align-items: flex-end;
+  pointer-events: all;
+  ${breakpoint('tablet')`
+    display: none;
+  `}
 `
 const ModifiedDate = styled.p`
   color: ${props => props.theme.textGrey};
@@ -152,6 +242,8 @@ const ButtonWithIcon = styled.button`
   cursor: pointer;
   margin-bottom: 24px;
   height: 1em;
+  ${breakpoint('mobile', 'tablet')`
+  `}
   p, div {
     padding: 0;
     margin: 0;
@@ -194,6 +286,19 @@ const Tipe = React.forwardRef<Editor, TipeProps>((props: TipeProps, ref: any) =>
       editorRef.current.focusAtEnd()
     }
   }, [editorRef, props.index])
+
+  const scrollerRef = useRef<HTMLDivElement>(null)
+  const scrolleeRef = useRef<HTMLDivElement>(null)
+  const onScroll = () => {
+    if (scrolleeRef.current && scrollerRef.current) {
+      if (scrollerRef.current.scrollLeft === 0) {
+        scrolleeRef.current.style.opacity = '0'
+      } else {
+        scrolleeRef.current.style.opacity = Math.min(scrollerRef.current.scrollLeft / (scrollerRef.current.scrollWidth - scrollerRef.current.clientWidth) + 0.3, 1.0).toString()
+        scrolleeRef.current.style.transform = 'scale(' + (0.8 + (scrollerRef.current.scrollLeft / (scrollerRef.current.scrollWidth - scrollerRef.current.clientWidth) * 0.2)).toString() + ')'
+      }
+    }
+  }
 
   let textTimeout: ReturnType<typeof setTimeout>
   const onTextChange = (valueFunc: () => string) => {
@@ -293,7 +398,7 @@ const Tipe = React.forwardRef<Editor, TipeProps>((props: TipeProps, ref: any) =>
   return <TipeContainer>
     {redirect && <Redirect push to={redirectPath} />}
     <Texts>
-      {(threadOfThisTipe && props.indexOfThisThread === -1) &&
+      {(threadOfThisTipe && indexInThread && props.indexOfThisThread === -1) &&
         <ThreadBar
           onClick={() => handleRedirect('/thread/' + library.tipes[props.index].thread)}>
           <IconThread />
@@ -305,7 +410,7 @@ const Tipe = React.forwardRef<Editor, TipeProps>((props: TipeProps, ref: any) =>
             ? <PreviousTipePreview></PreviousTipePreview>
             : <PreviousTipePreview>
               <p>•</p>
-              <p className="previous-text">{indexInThread &&
+              <p className="previous-text">{
               library.tipes.find((element) => {
                 return element.id === threadOfThisTipe.children[indexInThread + 1]
               })?.text}</p></PreviousTipePreview>
@@ -324,6 +429,40 @@ const Tipe = React.forwardRef<Editor, TipeProps>((props: TipeProps, ref: any) =>
         placeholder={placeholder}
       />
     </Texts>
+    <MobileTexts ref={scrollerRef} onScroll={onScroll}>
+      <MobileEditorWrap>
+        {(threadOfThisTipe && indexInThread && props.indexOfThisThread === -1) &&
+        <ThreadBar
+          onClick={() => handleRedirect('/thread/' + library.tipes[props.index].thread)}>
+          <IconThread />
+          <p>{threadOfThisTipe?.title === ''
+            ? <span>スレッド</span>
+            : <span>{threadOfThisTipe.title}</span>}
+          </p>
+          { threadOfThisTipe?.children.findIndex((element) => { return element === library.tipes[props.index].id }) === threadOfThisTipe.children.length - 1
+            ? <PreviousTipePreview></PreviousTipePreview>
+            : <PreviousTipePreview>
+              <p>•</p>
+              <p className="previous-text">{
+              library.tipes.find((element) => {
+                return element.id === threadOfThisTipe.children[indexInThread + 1]
+              })?.text}</p></PreviousTipePreview>
+          }
+          <IconForward />
+        </ThreadBar>
+        }
+        <Editor
+          ref={editorRef}
+          key={library.tipes[props.index].lastSessionId}
+          defaultValue={library.tipes[props.index].text}
+          onChange={value => onTextChange(value)}
+          onKeyDown={handleKeydown}
+          readOnly={props.readonly}
+          autoFocus={false}
+          placeholder={placeholder}
+        /></MobileEditorWrap>
+      <MobileTextsSpacer />
+    </MobileTexts>
     <Titles>
       <Spacer />
       <Sticky>
@@ -378,7 +517,59 @@ const Tipe = React.forwardRef<Editor, TipeProps>((props: TipeProps, ref: any) =>
           <IconThreadMore />
         </ButtonWithIcon>
       </Sticky>
+
+      <MobileStickyBottom>
+        <TitleInput
+          index={props.index}
+          key={library.tipes[props.index].lastSessionId}
+          onTitleChange={onTitleChange} />
+        <ModifiedDate className={'hiding modified-date'}>
+          {new Date(Number(library.tipes[props.index].editDate)).getHours()}:
+          {new Date(Number(library.tipes[props.index].editDate)).getMinutes()}:
+          {new Date(Number(library.tipes[props.index].editDate)).getSeconds()}
+        </ModifiedDate>
+      </MobileStickyBottom>
     </Titles>
+    <MobileTitlesUnder>
+      <MobileStickyMiddle ref={scrolleeRef} >
+        {library.tipes[props.index].thread === null &&
+          <ButtonWithIcon
+            tabIndex={0}
+            className={'button-with-icon'}
+            onClick={() => {
+              const newThread = newThreadState()
+              dispatch(addThread(newThread))
+              dispatch(addTipeToThread({
+                threadIndex: 0,
+                childIndexInThread: 0,
+                childIndexInTipes: props.index,
+                value: library.tipes[props.index].id
+              }))
+              dispatch(pushTipeToFirebase(props.index))
+              dispatch(pushThreadToFirebase(0))
+              handleRedirect('/thread/' + newThread.id)
+            }}>
+            <p>スレッドで返信</p>
+            <IconAddThread />
+          </ButtonWithIcon>
+        }
+        {(library.tipes[props.index].thread !== null && props.indexOfThisThread === -1) &&
+          <ButtonWithIcon
+            tabIndex={0}
+            className={'button-with-icon'}
+            onClick={() => handleRedirect('/thread/' + library.tipes[props.index].thread)}>
+            <p>スレッドに移動</p>
+            <IconForward />
+          </ButtonWithIcon>
+        }
+        <ButtonWithIcon
+          tabIndex={0}
+          className={'button-with-icon'}>
+          <p>既存のスレッドに追加</p>
+          <IconThreadMore />
+        </ButtonWithIcon>
+      </MobileStickyMiddle>
+    </MobileTitlesUnder>
   </TipeContainer>
 })
 
